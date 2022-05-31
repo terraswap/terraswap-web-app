b8cd1e1ba30ac62012e06ae3e00247a6d69c01b4
maro <maro@delightlabs.io>
2022-05-31 18:38:15 +0900

    hotfix: change the endpoint of API to get tx

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -25,7 +25,7 @@ export const MAX_MSG_LENGTH = 1024
 
 /* network:settings */
 export const TX_POLLING_INTERVAL = 1000
-export const MAX_TX_POLLING_RETRY = 20
+export const MAX_TX_POLLING_RETRY = 35
 
 export const socialMediaList = [
   {
